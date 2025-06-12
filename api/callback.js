60b53b97a52996e0fb2bacaefa1efc6d03eebd89
zaorinu-utils <luis.antonio15112009+githubrusinautils@gmail.com>
2025-06-12 12:09:42 +0000

    CORS Added

--- api/callback.js
+++ api/callback.js
@@ -1,6 +1,8 @@
 import { gerarToken } from './_jwt.js';
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://zaorinu-utils.github.io');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   const code = req.query.code;
   if (!code) return res.status(400).send('Código ausente');
 
